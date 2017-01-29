a4711210a0cb2d59237425c77f63dd9e66414055
Kenta Iwasaki <dranithix@gmail.com>
2017-01-29 15:56:12 +0800

    Created example for exam and problemset generation.

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,7 +6,7 @@ import {
     REGISTER_FAILURE,
     CURRENT_PROBLEM_SET,
     RETRIEVING_PROBLEM_SET,
-    GET_EXAMS,
+    GET_EXAMS
 } from "./types";
 import app from "../app";
 
@@ -14,6 +14,21 @@ const userService = app.service('users');
 const examService = app.service('exams');
 const problemsetService = app.service('problemsets');
 const commentService = app.service('comments');
+
+app.authenticate().then(() => {
+    examService.create({url: "http://web.pdx.edu/~erdman/CALCULUS/CALCULUS_pdf.pdf", categories: ["Calculus", "Math"]})
+        .then(exam => {
+            const examId = exam._id;
+            for (let i = 0; i < 5; i++) {
+                problemsetService.create({
+                    pageNumber: i,
+                    exam: examId,
+                    tags: ["Test Tag"]
+                }).then(problemset => console.log(problemset))
+                    .catch(err => console.error(err));
+            }
+        }).catch(err => console.error(err));
+});
 
 export const setLoading = (isLoading) => {
     return {
@@ -41,13 +56,16 @@ export const setSearchTerm = (term) => {
 // };
 
 export const createProblemSet = () => {
-  return (dispatch) => {
-    problemsetService.create({url: "https://ocw.mit.edu/courses/electrical-engineering-and-computer-science/6-046j-introduction-to-algorithms-sma-5503-fall-2005/exams/prac_final_sol.pdf", categories: []})
-      .then(res => {
-        console.log('EXAM: ', res);
-      })
-      .catch(err => console.error(err));
-  };
+    return (dispatch) => {
+        problemsetService.create({
+            url: "https://ocw.mit.edu/courses/electrical-engineering-and-computer-science/6-046j-introduction-to-algorithms-sma-5503-fall-2005/exams/prac_final_sol.pdf",
+            categories: []
+        })
+            .then(res => {
+                console.log('EXAM: ', res);
+            })
+            .catch(err => console.error(err));
+    };
 };
 
 export const getProblemSets = () => {
@@ -63,21 +81,21 @@ export const getProblemSets = () => {
 
     return (dispatch) => {
         problemsetService.find({$limit: 10, $skip: 0}).then(problemsets => {
-          console.log('PROBLEM SETSSS: ', problemsets);
-          dispatch({type: PROBLEM_SETS, payload: problemsets.data})
+            console.log('PROBLEM SETSSS: ', problemsets);
+            dispatch({type: PROBLEM_SETS, payload: problemsets.data})
         }).catch(err => console.error(err));
     };
 };
 
 export const getExams = () => {
-  return (dispatch) => {
-    examService.find({$limit: 10, $skip: 0})
-      .then(({ data }) => {
+    return (dispatch) => {
+        examService.find({$limit: 10, $skip: 0})
+            .then(({data}) => {
 
-        dispatch({ type: GET_EXAMS, payload: data})
-      })
-      .catch(err => console.error(err));
-  };
+                dispatch({type: GET_EXAMS, payload: data})
+            })
+            .catch(err => console.error(err));
+    };
 };
 
 export const currentProblemSet = (set) => {
@@ -131,14 +149,18 @@ export const postComment = (user) => {
             text: "Comment text",
             commenter: "User's ID",
             problemset: "Problemset ID"
-        }).then(comment => {})
-            .catch(err => {});
+        }).then(comment => {
+        })
+            .catch(err => {
+            });
     };
 };
 
 export const comments = (user, set) => {
     return (dispatch) => {
-        commentService.find({problemset: set._id}).then(comments => {}).catch(err => {});
+        commentService.find({problemset: set._id}).then(comments => {
+        }).catch(err => {
+        });
     };
 };
 
@@ -146,7 +168,9 @@ export const rateProblemSet = (user, set) => {
     return (dispatch) => {
         set.upvotes.push(user._id);
         problemsetService.update(set._id, {upvotes: set.upvotes})
-            .then(problemset => {}).catch(err => {});
+            .then(problemset => {
+            }).catch(err => {
+        });
     };
 };
 
@@ -154,6 +178,8 @@ export const rateComment = (user, set, comment) => {
     return (dispatch) => {
         comment.upvotes.push(user._id);
         commentService.update(comment._id, {upvotes: comment.upvotes})
-            .then(comment => {}).catch(err => {});
+            .then(comment => {
+            }).catch(err => {
+        });
     };
 };
